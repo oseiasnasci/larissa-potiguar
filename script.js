00dda3ce74121da57c456a7e975509f9a80fe59f
Oseias Nascimento <oseiassaboia1@gmail.com>
2023-10-12 10:15:15 -0300

    inicial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function changeColorRightFace() {
+  var cor = document.getElementById('colorRightFace').value
+  document.getElementById('divRightFace').style.backgroundColor = cor
+}
+
+function changeColorTopFace() {
+  var cor = document.getElementById('colorTopFace').value
+  document.getElementById('divTopFace').style.backgroundColor = cor
+}
+
+function changeColorLeftFace() {
+  var cor = document.getElementById('colorLeftFace').value
+  document.getElementById('divLeftFace').style.backgroundColor = cor
+}
+
+function changeColorBackFace() {
+  var cor = document.getElementById('colorBackFace').value
+  document.getElementById('divBackFace').style.backgroundColor = cor
+}
+
+function changeColorBottomFace() {
+  var cor = document.getElementById('colorBottomFace').value
+  document.getElementById('divBottomFace').style.backgroundColor = cor
+}
